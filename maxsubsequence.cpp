255c04a71f5165df788057985f15043fcc43f45e
wearenothing <39990563+wearenothing@users.noreply.github.com>
2019-04-05 00:14:54 +0800

    Update maxsubsequence.cpp

--- maxsubsequence.cpp
+++ maxsubsequence.cpp
@@ -1,8 +1,30 @@
+// maxsubsequence.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include "pch.h"
 #include <iostream>
 using namespace std;
+bool isf(int a[], int n)
+{
+	bool flag0=false;
+	bool flag1 = false;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > 0)
+			return false;
+		else if (a[i] == 0)
+			flag0 = true;
+		else
+			flag1 = true;
+	}
+	if (flag0&&flag1)
+		return true;
+	else
+		return false;
+}
 int MaxSubSequence(int a[], int n, int *right, int *left)
 {
+	
 	int maxsum, thisum;
 	maxsum = thisum = 0;
 	*right = a[n-1];
@@ -38,8 +60,16 @@ int main()
 		cin >> a[i];
 	}
 
+	if (isf(a,k))
+	{
+		cout << 0 << " " << 0 << " " << 0 << endl;
+	}
+	else
+	{
 	cout << MaxSubSequence(a, k, &right, &left) << " ";
 	cout << left << " " << right << endl;
+	}
+	
 }
 
 
