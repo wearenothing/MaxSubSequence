201f4bd15f28eacacd24b68a2c137a7e3f3ee4f6
wearenothing <39990563+wearenothing@users.noreply.github.com>
2019-04-04 18:56:25 +0800

    Update maxsubsequence.cpp
    not pass:only minus and zero

--- maxsubsequence.cpp
+++ maxsubsequence.cpp
@@ -1,33 +1,45 @@
-// maxsubsequence.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 using namespace std;
-int MaxSubSequence(int a[], int n)
+int MaxSubSequence(int a[], int n, int *right, int *left)
 {
-	int maxsum , thisum ;
+	int maxsum, thisum;
 	maxsum = thisum = 0;
+	*right = a[n-1];
+	*left = a[0];
+	int thisleft = 0;
 	for (int i = 0; i < n; i++)
 	{
 		thisum += a[i];
 		if (thisum > maxsum)
+		{
 			maxsum = thisum;
+			*right = a[i];
+			*left = a[thisleft];
+		}
+
 		else if (thisum < 0)
+		{
 			thisum = 0;
+			thisleft = i + 1;
+		}
+
 	}
 	return maxsum;
 }
 int main()
 {
-	int k ;
+	int k;
 	int a[100000];
-	cin >> k; 
-	for(int i=0;i<k;i++)
+	int right, left;
+	cin >> k;
+	for (int i = 0; i < k; i++)
 	{
 		cin >> a[i];
 	}
-	
-	cout << MaxSubSequence(a, k) << endl;
+
+	cout << MaxSubSequence(a, k, &right, &left) << " ";
+	cout << left << " " << right << endl;
 }
+
 
